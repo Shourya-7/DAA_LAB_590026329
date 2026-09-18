f48d0577525f6b6076c4c595f4e73f5ef21eb474
Shourya-7 <shourya3477@gmail.com>
2026-09-18 09:03:54 +0530

    Create 1.c

---
+++ Exp_4/1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+
+    int n, i;
+    int a = 0, b = 1, c;
+
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
+
+    printf("Fibonacci Series: ");
+
+    for (i = 0; i < n; i++) {
+
+        printf("%d ", a);
+
+        c = a + b;
+        a = b;
+        b = c;
+    }
+
+    printf("\n\nName: Shourya Singh\n");
+
+    return 0;
+}
