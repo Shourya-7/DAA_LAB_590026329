45b9befac916d68c2d1d0aa7f5effdc7f6b1b1ed
Shourya-7 <shourya3477@gmail.com>
2026-09-18 10:34:17 +0530

    Implement merge sort algorithm in 1.c
    Added merge and mergeSort functions to sort an array.

--- Exp_4/1.c
+++ Exp_4/1.c
@@ -1,22 +1,75 @@
 #include <stdio.h>
 
+void merge(int arr[], int low, int mid, int high) {
+    int i = low;
+    int j = mid + 1;
+    int k = 0;
+    int temp[100];
+
+    while (i <= mid && j <= high) {
+        if (arr[i] <= arr[j]) {
+            temp[k] = arr[i];
+            i++;
+        } else {
+            temp[k] = arr[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i <= mid) {
+        temp[k] = arr[i];
+        i++;
+        k++;
+    }
+
+    while (j <= high) {
+        temp[k] = arr[j];
+        j++;
+        k++;
+    }
+
+    for (i = low, k = 0; i <= high; i++, k++) {
+        arr[i] = temp[k];
+    }
+}
+
+void mergeSort(int arr[], int low, int high) {
+    if (low < high) {
+        int mid = (low + high) / 2;
+
+        mergeSort(arr, low, mid);
+        mergeSort(arr, mid + 1, high);
+
+        merge(arr, low, mid, high);
+    }
+}
+
 int main() {
-
+    int arr[100];
     int n, i;
-    int a = 0, b = 1, c;
 
-    printf("Enter the number of terms: ");
+    printf("Enter the number of elements: ");
     scanf("%d", &n);
 
-    printf("Fibonacci Series: ");
+    printf("Enter the elements:\n");
 
     for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
 
-        printf("%d ", a);
+    printf("\nOriginal array:\n");
 
-        c = a + b;
-        a = b;
-        b = c;
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    mergeSort(arr, 0, n - 1);
+
+    printf("\n\nSorted array:\n");
+
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
     }
 
     printf("\n\nName: Shourya Singh\n");
