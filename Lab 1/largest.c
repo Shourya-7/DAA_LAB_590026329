672842dbbb23c431426acb066c34182a1382ceca
Shourya-7 <shourya3477@gmail.com>
2026-09-18 10:52:56 +0530

    Create largest.c

---
+++ Lab 1/largest.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    int arr[100];
+    int largest;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter the elements:\n");
+
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    largest = arr[0];
+
+    for (i = 1; i < n; i++) {
+        if (arr[i] > largest) {
+            largest = arr[i];
+        }
+    }
+
+    printf("\nLargest number = %d\n", largest);
+
+    printf("\nName: Shourya Singh\n");
+
+    return 0;
+}
