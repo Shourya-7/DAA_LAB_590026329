9855c32036c3b87b125a66fbd54d114622b23d50
Shourya-7 <shourya3477@gmail.com>
2026-09-18 08:51:20 +0530

    Implement binary search algorithm in C

---
+++ Exp_3/1.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int binarySearch(int arr[], int n, int target) {
+
+    int low = 0;
+    int high = n - 1;
+
+    while (low <= high) {
+
+        int mid = (low + high) / 2;
+
+        if (arr[mid] == target) {
+            return mid;
+        }
+
+        else if (target < arr[mid]) {
+            high = mid - 1;
+        }
+
+        else {
+            low = mid + 1;
+        }
+    }
+
+    return -1;
+}
+
+int main() {
+
+    int arr[] = {2, 5, 8, 12, 16, 23, 38, 45, 56, 72};
+
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    int target = 38;
+
+    int result = binarySearch(arr, n, target);
+
+    if (result != -1) {
+        printf("Element found at index %d\n", result);
+    }
+    else {
+        printf("Element not found\n");
+    }
+
+    printf("\nName: Shourya Singh\n");
+
+    return 0;
+}
